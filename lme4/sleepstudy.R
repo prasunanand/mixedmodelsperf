625724b8042f602dcd6dbfb07b69ecec9254981b
prasun <prasunanand.bitsp@gmail.com>
2016-07-13 01:36:22 +0530

    sleep study example

---
+++ lme4/sleepstudy.R
@@ -0,0 +1,10 @@
+library(lme4)
+sleepstudy <- load("../data/sleepstudy.rda")
+str(sleepstudy)
+require(lattice)
+xyplot(Reaction ~ Days | Subject, sleepstudy, type = c("g","p","r"),
+       index = function(x,y) coef(lm(y ~ x))[1],
+       xlab = "Days of sleep deprivation",
+       ylab = "Average reaction time (ms)", aspect = "xy")
+(fm1 <- lmer(Reaction ~ Days + (Days|Subject), sleepstudy))
+(fm2 <- lmer(Reaction ~ Days + (1|Subject) + (0+Days|Subject), sleepstudy))
