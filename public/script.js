fb17f543d28073569b953ca53e287a5979b7bead
prasun <prasunanand.bitsp@gmail.com>
2016-07-14 03:20:06 +0530

    graphs: setup sinatra

---
+++ public/script.js
@@ -0,0 +1,134 @@
+var app = angular.module('resultApp',[]);
+
+app.controller('MainCtrl', function($scope, $http) {
+
+  resultRubyUrl = "ruby.json"
+  resultJavaRubyUrl = "jruby.json"
+  resultLapackeRubyUrl = "ruby-lapacke.json"
+
+
+  $scope.checked= false;
+  $http.get(resultRubyUrl).success(function(data) {
+     $http.get(resultJavaRubyUrl).success(function(data2) {
+      $http.get(resultLapackeRubyUrl).success(function(data3) {
+    $scope.charts = data.features;
+    data.features.jruby = data2.features.jruby
+    data.features.rubyLapacke = data3.features.ruby
+    console.log($scope.charts);
+
+    for (key in $scope.charts.jruby){
+      // console.log($scope.charts.features[key])
+      $(function () {
+          // alert('#chart'+key)
+          $('#chart'+key).highcharts({
+
+              type: 'line',
+              title: {
+                  text: 'Matrix '+key,
+                  x: -20 //center
+              },
+              subtitle: {
+                  x: -20
+              },
+              xAxis: {
+                title: {
+                  text: 'Number of elements in one Matrix'
+                },
+              },
+              yAxis: {
+                  title: {
+                      text: 'Time (s)'
+                  },
+                  plotLines: [{
+                      value: 0,
+                      width: 1,
+                      color: '#808080'
+                  }]
+              },
+              tooltip: {
+                  valueSuffix: 's'
+              },
+              legend: {
+                  layout: 'vertical',
+                  align: 'right',
+                  verticalAlign: 'middle',
+                  borderWidth: 0
+              },
+              series: [{
+                  name: "ruby",
+                  data: $scope.charts.ruby[key]
+                },{
+                  name: "jruby",
+                  data: $scope.charts.jruby[key]
+                },{
+                  name: "ruby-lapacke",
+                  data: $scope.charts.rubyLapacke[key]
+                },
+              ]
+        });
+        });
+    }
+  });
+  });
+  });
+
+  // $http.get(resultJavaRubyUrl).success(function(data) {
+  //   $scope.charts = data;
+  //   console.log(data);
+  // });
+})
+  // $(function () {
+//   $http.get(resultRubyUrl).success(function(data) {
+//   $http.get(resultJavaRubyUrl).success(function(data2) {
+//     $('#container').highcharts({
+
+//         type: 'line',
+//         title: {
+//             text: 'Matrix addition',
+//             x: -20 //center
+//         },
+//         subtitle: {
+//             x: -20
+//         },
+//         xAxis: {
+//             tickInterval: 1,
+//             categories: [10,1000,100000,100000000]
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Time (ms)'
+//             },
+//             plotLines: [{
+//                 value: 0,
+//                 width: 1,
+//                 color: '#808080'
+//             }]
+//         },
+//         tooltip: {
+//             valueSuffix: '°C'
+//         },
+//         legend: {
+//             layout: 'vertical',
+//             align: 'right',
+//             verticalAlign: 'middle',
+//             borderWidth: 0
+//         },
+//         series: [{
+//             name: "addition",
+//             data: data.ruby.addition
+//           },
+//           {
+//             name : "jrubyadd",
+//             data : data2.jruby.addition
+//           }
+//         ]
+//     });
+//     });
+//   });
+//   });
+
+
+// });
+
+resultRubyUrl = "ruby.json"
+resultJavaRubyUrl = "jruby.json"
---
+++ serve.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'json'
+
+# file = File.read('./results.json')
+
+get '/' do
+  erb :index
+end
